8ad5ce16fac4885989f04cdba68c5b0ccda5a316
Dusan Matejka <matejkadusan32@gmail.com>
2016-08-17 10:16:45 +0200

    Check for vmtools availability and parse cmd arg into list

--- lib/modules/execute.py
+++ lib/modules/execute.py
@@ -16,15 +16,19 @@ class ExecCommands(BaseCommands):
     @args('--name', required=True, help='name of a object where to run command')
     @args('--guest-user', help="guest's user under which to run command")
     @args('--guest-pass', help="guest user's password")
-    @args('--cmd', help="commands to execute e.g. --cmd ['cmd1', 'cmd2']", type=list)
+    @args('--cmd', help="commands to execute e.g. --cmd 'cmd1; cmd2'", type=str)
     def execute(self, args):
         try:
-            self.exec_inside_vm(args.name, args.cmd, args.guest_user, args.guest_pass, wait_for_tools=True)
+            # When this method is executed, command was called directly from cmd line and content of cmd argument,
+            # separated by semicolon, need to be converted into list
+            args.cmd = args.cmd.split(';')
+            self.exec_inside_vm(args.name, args.cmd, args.guest_user, args.guest_pass, wait_for_tools=False)
         except VmCLIException as e:
             self.exit(e.message, errno=4)
 
     def exec_inside_vm(self, name, commands, guest_user=None, guest_pass=None, wait_for_tools=False):
         """Runs provided command inside guest's operating system."""
+        self.logger.info('Loading required VMware resources...')
         guest_user = guest_user or conf.VM_GUEST_USER
         guest_pass = guest_pass or conf.VM_GUEST_PASS
         vm = self.get_obj('vm', name)
@@ -32,10 +36,11 @@ class ExecCommands(BaseCommands):
             raise VmCLIException('No command provided for execution!')
 
         self.logger.info("Checking if guest's OS has vmtools installed ...")
-        if vm.guest.toolsStatus in ['toolsNotInstalled', 'toolsNotRunning'] and not wait_for_tools:
-            raise VmCLIException("Guest's VMware tools are not installed or not running. Cannot continue")
-        elif wait_for_tools:
+        if wait_for_tools:
             self.wait_for_guest_vmtools(vm)
+
+        if vm.guest.toolsStatus in ['toolsNotInstalled', 'toolsNotRunning']:
+            raise VmCLIException("Guest's VMware tools are not installed or not running. Aborting...")
 
         try:
             credentials = vim.vm.guest.NamePasswordAuthentication(username=guest_user, password=guest_pass)
