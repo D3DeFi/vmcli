251c835c0bcbf7550039c916f13388205f007b70
Dusan Matejka <matejkadusan32@gmail.com>
2017-04-02 18:05:37 +0200

    ported execute module to new argument parsing logic

--- lib/modules/execute.py
+++ lib/modules/execute.py
@@ -4,8 +4,6 @@ from lib.modules import BaseCommands
 from lib.tools.argparser import args
 from lib.exceptions import VmCLIException
 
-import lib.config as conf
-
 
 class ExecCommands(BaseCommands):
     """execute commands inside vm's guest operating system."""
@@ -14,13 +12,13 @@ class ExecCommands(BaseCommands):
         super(ExecCommands, self).__init__(*args, **kwargs)
 
     @args('--name', required=True, help='name of a object where to run command')
-    @args('--guest-user', help="guest's user under which to run command")
-    @args('--guest-pass', help="guest user's password")
+    @args('--guest-user', help="guest's user under which to run command", map='VM_GUEST_USER')
+    @args('--guest-pass', help="guest user's password", map='VM_GUEST_PASS')
     @args('--cmd', help="commands to execute e.g. --cmd 'cmd1; cmd2'", type=str)
     def execute(self, args):
         try:
             # When this method is executed, command was called directly from cmd line and content of cmd argument,
-            # separated by semicolon, need to be converted into list
+            # separated by semicolon, needs to be converted into list
             args.cmd = args.cmd.split(';')
             self.exec_inside_vm(args.name, args.cmd, args.guest_user, args.guest_pass, wait_for_tools=False)
         except VmCLIException as e:
@@ -29,8 +27,6 @@ class ExecCommands(BaseCommands):
     def exec_inside_vm(self, name, commands, guest_user=None, guest_pass=None, wait_for_tools=False):
         """Runs provided command inside guest's operating system."""
         self.logger.info('Loading required VMware resources...')
-        guest_user = guest_user or conf.VM_GUEST_USER
-        guest_pass = guest_pass or conf.VM_GUEST_PASS
         vm = self.get_obj('vm', name)
         if not commands:
             raise VmCLIException('No command provided for execution!')
