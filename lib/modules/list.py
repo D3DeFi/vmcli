5f4ba2db5328018aaab22d2ee2cd6a627b47988a
Dusan Matejka <matejkadusan32@gmail.com>
2017-12-18 15:03:04 +0100

    Expanded show function to print some relevant information about VMs

--- lib/modules/list.py
+++ lib/modules/list.py
@@ -1,5 +1,8 @@
+from pyVmomi import vim
+
 from lib.modules import BaseCommands
 from lib.tools.argparser import args
+from lib.tools import convert_to_mb
 from lib.constants import VMWARE_TYPES
 
 
@@ -26,10 +29,87 @@ class ListCommands(BaseCommands):
     @args('--name', help='search for a specific object instead')
     def show_item(self, vimtype, name):
         """Lists details about specific VMware object."""
-        # TODO: format output
         obj = self.get_obj(vimtype, name, default=False)
-        if obj:
+        if not obj:
+            return
+
+        if vimtype == 'vm':
+            self.show_vm_details(obj)
+        else:
+            # TODO: format output
             print(obj.summary)
 
+    def show_vm_details(self, obj):
+        """Lists details about VM."""
+        datacenter, folder, root_obj = self.get_vm_datacenter_details(obj)
+        datastore = self.get_vm_datastore_name(obj.datastore[0].name, root_obj)
+        resource_pool = obj.resourcePool.summary.name
+        mem = obj.summary.config.memorySizeMB
+        cpu = obj.summary.config.numCpu
+        cluster = self.get_vm_cluster_name(obj.summary.runtime.host.name, root_obj)
+
+        print('Datacenter ........ {}'.format(datacenter))
+        print('Folder ............ {}'.format(folder))
+        print('Datastore ......... {}'.format(datastore))
+        print('Cluster ........... {}'.format(cluster))
+        print('ResourcePool ...... {}'.format(resource_pool))
+        print('Memory ............ {}'.format(mem))
+        print('CPU ............... {}'.format(cpu))
+        # Get information about hard drives
+        for device in obj.config.hardware.device:
+            if isinstance(device, vim.vm.device.VirtualDisk):
+                devsize = device.deviceInfo.summary
+                dshost = self.get_vm_datastore_name(device.backing.datastore.name, root_obj)
+                devsize = convert_to_mb(devsize.replace(',', '').rstrip('B'))
+                print('HDD ............... {}M ({})'.format(devsize, dshost))
+
+        for nic in obj.network:
+            print('Network ........... {}'.format(nic.summary.name))
+
+    def get_vm_datacenter_details(self, obj):
+        """Retrieves VM's folder, dc and root parent object by traversing linked list of parents back to beginning."""
+        folder = 'N/A'
+        datacenter = 'N/A'
+        root_obj = False
+
+        if obj.parent:
+            folder = obj.parent.name
+            obj_parent = obj.parent
+            while obj_parent:
+                root_obj = obj_parent
+                # Datacenter is the latest parent
+                if isinstance(obj_parent, vim.Datacenter):
+                    datacenter = obj_parent.name
+                    break
+
+                obj_parent = obj_parent.parent
+
+        return (datacenter, folder, root_obj)
+
+    def get_vm_datastore_name(self, vm_ds_node, root_obj):
+        """Retrieves name of main datastore, where VM is placed. If possible, name of datastore cluster is returned."""
+        datastore = 'N/A'
+        for dshost in root_obj.datastoreFolder.childEntity:
+            if isinstance(dshost, vim.StoragePod):
+                for ds in dshost.childEntity:
+                    if ds.name == vm_ds_node:
+                        return dshost.name
+
+            elif dshost.name == vm_ds_node:
+                return dshost.name
+        return datastore
+
+    def get_vm_cluster_name(self, vm_cl_node, root_obj):
+        """Retrieves Compute Cluster or Node name by iterating over Compute nodes."""
+        cluster = 'N/A'
+        for clhost in root_obj.hostFolder.childEntity:
+            if isinstance(clhost, vim.ClusterComputeResource):
+                for cl in clhost.host:
+                    if cl.name == vm_cl_node:
+                        return clhost.name
+
+            elif clhost.name == vm_cl_node:
+                return clhost.name
+        return cluster
 
 BaseCommands.register('list', ListCommands)
--- lib/tools/__init__.py
+++ lib/tools/__init__.py
@@ -2,9 +2,8 @@ from lib.constants import VM_MIN_MEM, VM_MAX_MEM
 from lib.exceptions import VmCLIException
 
 
-def normalize_memory(value):
-    """Function converts passed value to integer, which will represent size in megabytes
-    as well as performs control whether the value sits between global limits."""
+def convert_to_mb(value):
+    """Converts size to megabytes."""
 
     if isinstance(value, str):
         if value.endswith('T'):
@@ -13,12 +12,25 @@ def normalize_memory(value):
             value = int(value.strip('G')) * 1024
         elif value.endswith('M'):
             value = int(value.strip('M'))
+        elif value.endswith('K'):
+            value = int(value.strip('K')) / 1024
+        # Assume bytes otherwise
+        else:
+            value = int(value) / 1024 / 1024
 
     try:
         value = int(value)
     except ValueError:
         raise VmCLIException('Unable to convert memory size to gigabytes. Aborting...')
 
+    return value
+
+
+def normalize_memory(value):
+    """Function converts passed value to integer, which will represent size in megabytes
+    as well as performs control whether the value sits between global limits."""
+
+    value = convert_to_mb(value)
     if value < VM_MIN_MEM or value > VM_MAX_MEM:
         raise VmCLIException('Memory must be between {}-{} megabytes'.format(VM_MIN_MEM, VM_MAX_MEM))
     else:
